5aa3e37ce3ea8fb6f78fd780a9974873216d8a79
tattiP <potatiana5@gmail.com>
2024-04-07 13:39:06 -0400

    Создан класс Triangle. Реализованы методы для вычисления периметра и площади треугольника. Сделаны тесты для проверки, что методы правильно работают. Исправлены данные в geometry."

--- sandbox/src/main/java/com/tatiana/geometry/Geometry.java
+++ sandbox/src/main/java/com/tatiana/geometry/Geometry.java
@@ -2,6 +2,7 @@ package com.tatiana.geometry;
 
 import com.tatiana.geometry.figures.Rectangle;
 import com.tatiana.geometry.figures.Square;
+import com.tatiana.geometry.figures.Triangle;
 
 public class Geometry {
     public static void main(String[] args) {
@@ -12,6 +13,8 @@ public class Geometry {
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(7.0, 9.0);
 
+        Triangle.printTrianglePerimeter(10.0, 10.0, 15.0);
+        Triangle.printTriangleArea(10.0, 10.0, 15.0);
 
     }
 
--- sandbox/src/main/java/com/tatiana/geometry/figures/Triangle.java
+++ sandbox/src/main/java/com/tatiana/geometry/figures/Triangle.java
@@ -1,9 +1,34 @@
 package com.tatiana.geometry.figures;
 
-public class Triangle {
+public record Triangle (double a, double b, double c) {
 
+
+    public static void printTrianglePerimeter(double a, double b, double c) {
+        var text = String.format("Периметр треугольника со сторонами %f и %f и %f = %f", a, b, c, Perimeter(a, b, c));
+        System.out.println(text); // в trianglePerimeter заложена формула треугольника: сначала ввели формулу, а потом правой кнопкой Refactor > Extract method > называем как нам будет понятней
+    }
+
+
+    public static void printTriangleArea(double a, double b, double c) {
+        var text = String.format("Площадь треугольника со сторонами %f и %f и %f " + " = " + Math.sqrt(triangleArea(a, b, c)));
+        System.out.println(text);
+    }
+
+    public static double Perimeter(double a, double b, double c) {
+        return a + b +c;
+    }
+
+    private static double semiPerimeter(double a, double b, double c) {
+        return (a + b + c)/2;
+    }
+
+    public static double triangleArea(double a, double b, double c) {
+        var p = semiPerimeter(a, b, c);
+        return p * (p - a) * (p - b) * (p - c);
+    }
 
 }
+
 
 
 
