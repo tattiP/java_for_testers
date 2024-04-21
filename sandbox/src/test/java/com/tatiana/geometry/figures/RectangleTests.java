bec31ec9429bbc5383ffa25497c2aaacd7f4a714
tattiP <potatiana5@gmail.com>
2024-04-21 17:31:43 -0400

    метод equals для сравнения объектов

--- sandbox/src/test/java/com/tatiana/geometry/figures/RectangleTests.java
+++ sandbox/src/test/java/com/tatiana/geometry/figures/RectangleTests.java
@@ -3,21 +3,32 @@ package com.tatiana.geometry.figures;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.awt.Rectangle;
-
 public class RectangleTests {
 
 
-      /*  @Test
-        void cannotCreateRectangleWithNegativeSide(){
-            try {
-                new Rectangle(-5.0, 3.0);
-                Assertions.fail();
-            } catch (IllegalArgumentException exception){
+    @Test
+    void cannotCreateRectangleWithNegativeSide() {
+        try {
+            new Rectangle(-5.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
 
-            }
+        }
 
-    }*/
+    }
 
+    @Test
+    void testEquality() {
+        var r1 = new Rectangle(5.0, 4.0); // var r1 - we create a new square
+        var r2 = new Rectangle(5.0, 4.0); // var r2 - we create another square
+        Assertions.assertEquals(r1, r2);
+    }
 
+    @Test
+    void testEquality2() {
+        var r1 = new Rectangle(5.0, 4.0); // var r1 - we create a new square
+        var r2 = new Rectangle(4.0, 5.0); // var r2 - we create another square
+        Assertions.assertEquals(r1, r2);
+
+    }
 }
--- sandbox/src/test/java/com/tatiana/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/com/tatiana/geometry/figures/SquareTests.java
@@ -31,9 +31,30 @@ public class SquareTests {
             new Square(-5.0);
             Assertions.fail();
         } catch (IllegalArgumentException exception){
-
+            // OK
         }
     }
+
+    @Test
+    void testEquality(){
+        var s1 = new Square(5.0); // var s1 - we create a new square
+        var s2 = new Square(5.0); // var s2 - we create another square
+        Assertions.assertEquals(s1, s2);
+    }
+
+    @Test
+    void testNonEquality(){
+        var s1 = new Square(5.0); // var s1 - we create a new square
+        var s2 = new Square(4.0); // var s2 - we create another square
+        Assertions.assertNotEquals(s1, s2);
+    }
+    @Test
+    void testPass(){
+        var s1 = new Square(5.0); // var s1 - we create a new square
+        var s2 = new Square(5.0); // var s2 - we create another square
+        Assertions.assertTrue(s1.equals(s2)); // s1 - в первом объкте вызываем метод и в качестве параметра указывеам второй объкт s2
+    }
+
 
 
 }
