48f6bd5d2a8ad055afa8fa25f2f97df69d46b98b
tattiP <potatiana5@gmail.com>
2024-05-05 15:45:36 -0400

    Created a Group object with empty parameters

--- addressbook_web_tests/src/test/java/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/GroupCreationTest.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTest extends TestBase {
@@ -5,17 +6,27 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void canCreateGroup() {
         openGroupsPage();
-        createGroup("group name", "group header", "group footer");
-
+        createGroup(new GroupData("group name", "group header", "group footer"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
 
 
     }
+
+
+
 
 }
 
