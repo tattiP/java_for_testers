aa383e450e142957684e66376d932fa998d66a22
tattiP <potatiana5@gmail.com>
2024-05-12 17:14:08 -0400

    New modifyGroupName test created and passed

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,14 +5,21 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
     protected WebDriver driver;
     private LoginHelper session; // session eto peremennaya. loginhelper will help to log in and log out (start session, exit session)
     private GroupHelper groups;
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if ("chrome".equals(browser)){
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)){
+                driver = new FirefoxDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit)); // when java finishes working call function "quit" in the object that is inside "driver" variable
             driver.get("http://localhost/addressbook/index.php");
             driver.manage().window().setSize(new Dimension(1210, 773));
